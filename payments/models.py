b158b780657f986590c43eb767bfd9901d9f8331
“Gbenga <dtechydude@gmail.com>
2025-06-08 14:23:08 +0100

    student payment_edit

--- payments/models.py
+++ payments/models.py
@@ -60,123 +60,9 @@ class PaymentChart(models.Model):
 
     class Meta:
         ordering:['-session']
+        verbose_name = "Payment Chart"
+        verbose_name_plural = "Payment Chart"
     
-
-
-# class PaymentDetail(models.Model):
-#     student_detail = models.ForeignKey(Student, on_delete=models.CASCADE, default=None, null=True)
-#     # user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=)
-#     # student_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True,  help_text='confirm student username', related_name='student_detail')
-#     payment_name = models.ForeignKey(PaymentChart, on_delete=models.CASCADE, default= None, related_name='payment_name')
-#     amount_paid_a = models.DecimalField(max_digits=15, decimal_places=2, default=0.0, null=True, help_text='First Payment Amount', verbose_name='1st Payment')
-#     bank_name_a = models.ForeignKey(BankDetail, on_delete=models.CASCADE, default=None, null=True, related_name='bank_name_a')   
-#     payment_date_a = models.DateField()
-#     remark_a = models.CharField(max_length=200, blank=True, verbose_name='description(if any)')
-
-#     amount_paid_b = models.DecimalField(max_digits=15, decimal_places=2, default=0.0, null=True, help_text='Second Payment Amount', verbose_name='2nd Payment')
-#     bank_name_b = models.ForeignKey(BankDetail, on_delete=models.CASCADE, default=None, null=True, blank=True, related_name='bank_name_b')   
-#     payment_date_b = models.DateField(blank=True, null=True)
-#     remark_b = models.CharField(max_length=200, blank=True, verbose_name='description(if any)')
-
-#     amount_paid_c = models.DecimalField(max_digits=15, decimal_places=2, default=0.0, null=True, help_text='Third Payment Amount', verbose_name='3rd Payment')
-#     bank_name_c = models.ForeignKey(BankDetail, on_delete=models.CASCADE, default=None, null=True, blank=True)   
-#     payment_date_c = models.DateField(blank=True, null=True)
-#     remark_c = models.CharField(max_length=200, blank=True, verbose_name='description(if any)')
-
-#     discount = models.DecimalField(help_text='enter in (%) leave empty if no discoun is given', max_digits=3, decimal_places=0, blank=True, null=True, verbose_name='TOTAL DISCOUNT(if any)', default=0, validators=[MinValueValidator(0), MaxValueValidator(100)]) 
-#     # payment confirmation
-#     confirmed_a = models.BooleanField(default=False) 
-#     confirmed_b = models.BooleanField(default=False) 
-#     confirmed_c = models.BooleanField(default=False) 
-
-#     trans_id = models.CharField(max_length=8, blank=True)
-
-#     payment_updated_date = models.DateField(auto_now_add=True)     
-
-#     class Meta:
-#         ordering = ['-student_detail' ]
-
-#         unique_together = ['student_detail', 'payment_name']
-
-        
-
-#     def __str__ (self):
-#        return f'{self.student_detail} '
-
-#     def get_absolute_url(self):
-#         return reverse('payments:my_payments')  
-    
-#     def save(self, *args, **kwargs):
-#         if self.trans_id =="":
-#             trans_id = generate_trans_id()
-#             self.trans_id = trans_id
-#         super().save(*args, **kwargs)
-    
-#     # def get_absolute_url(self):
-#     #     return reverse('payment:payment_detail', kwargs={'id':self.id})
-    
-       
-
-#     @property
-#     def balance_pay(self):
-#        return self.payment_name.amount_due - (self.amount_paid_a + self.amount_paid_b + self.amount_paid_c)
-    
-#     @property
-#     def total_amount_paid(self):
-#        return (self.amount_paid_a + self.amount_paid_b + self.amount_paid_c)
-
-#     @property
-#     def discounted_amount_due(self):
-#        return self.payment_name.amount_due - (self.discount/100 * self.payment_name.amount_due)
-    
-#     # balance after discount has been removed
-#     @property
-#     def discounted_balance_pay(self):
-#         #return self.discounted_amount_due - self.amount_paid
-#         return self.discounted_amount_due - self.total_amount_paid
-    
-#     # for getting total balance to pay if discounted
-#     @property
-#     def discounted_balance_owed(self):
-#         #return self.discounted_amount_due - self.amount_paid
-#         return self.discounted_amount_due - (self.amount_paid_a + self.amount_paid_b + self.amount_paid_c )
-    
-    
-#     # if student has CREDIT without discount
-#     @property
-#     def credit_balance(self):
-#         #return self.discounted_amount_due - self.amount_paid
-#         return self.total_amount_paid - self.payment_name.amount_due
-    
-#         # if student has CREDIT and is dicounted
-#     @property
-#     def credit_balance_discounted(self):        
-#         return self.total_amount_paid - self.discounted_amount_due
-    
-#     @property
-#     def discounted_amount(self):
-#         return self.payment_name.amount_due - self.discounted_balance_pay
-
-
-#     # for getting total number of payments a student made
-#     @property
-#     def no_of_payments(request):
-#       return PaymentDetail.objects.filter(student_detail = request.student_detail).count()
-    
-#     #debtor status
-#     @property
-#     def debt_stand(self):
-#         return self.payment_name.amount_due - (self.amount_paid_a + self.amount_paid_b + self.amount_paid_c )
-    
-#     #debtor status with DISCOUNT
-#     @property
-#     def debt_stand_discounted(self):
-#         return self.discounted_amount_due - (self.amount_paid_a + self.amount_paid_b + self.amount_paid_c )
-    
-    
-#     @property
-#     def studentdetail(self):
-#        return self.student_detail
 
 
 # self-payment module
@@ -212,6 +98,8 @@ class PaymentDetail1(models.Model):
         ordering = ['-payee' ]
 
         unique_together = ['payee', 'payment_name']
+        verbose_name = "Payment Detail"
+        verbose_name_plural = "Payment Detail"
 
         
 
